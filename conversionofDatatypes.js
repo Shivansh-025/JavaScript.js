d36860087c4963cb8b397f4f11804e33a43e0b9f
Your Name <your-email@example.com>
2024-12-18 19:56:03 +0530

    commited

---
+++ conversionofDatatypes.js
@@ -0,0 +1,35 @@
+let score = "Shivansh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+// let marks="34" //=>number,34
+//let marks="32abc" //=> number,NaN
+// let marks=true //=> number,1
+let marks= false //=number,0
+let valueInNumber = Number(marks)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+//let isLoggedIn = "shivansh" // =>true
+//let isLoggedIn=1 //=> true
+let isLoggedIn=0 //=>false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+  //console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "mailatshivm@gmail.com"
+var accountPassword = "12345"
+let accountCity="Varanasi"
+let accountState
+// accountId=23
+accountEmail="Shiva@gmail.com";
+accountPassword ="2345"
+accountCity="Delhi";
+/*
+we does not prefer use of var because of issue in block scope and functional scope
+ */
+console.table([accountId , accountEmail, accountPassword , accountCity, accountState]);
