24a63c06a4006e0584bc1075be2d2d1c2f38b442
Your Name <your-email@example.com>
2024-12-18 23:14:59 +0530

    comparisons and datatype summary done

---
+++ comparisons.js
@@ -0,0 +1,27 @@
+//console.log("2">1);//True
+
+
+/*Datatype nahi check hota jo bhi datatype ho use number me change karke boolean me output deta hai*/
+
+
+//console.log(1<false);//false
+
+
+/*null ko randomly kisi bhi number se replace karke comparison perform hota hai */ 
+
+
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//true
+
+
+// console.log(undefined==0);//false
+// console.log(undefined<0);//false
+// console.log(undefined>0);//false
+
+//=== Strictly datatype check(isme datatype  ko strictly check kiya jata hai aur comparison ki jati hai)
+
+console.log("2"===2);//false
+
+
+
