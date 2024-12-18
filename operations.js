27640f2f5570002a1e4bdbadbbe29ac93a872526
Your Name <your-email@example.com>
2024-12-18 21:40:27 +0530

    operations done

---
+++ operations.js
@@ -0,0 +1,61 @@
+let value=3
+let negativevalue=-value
+// console.log(negativevalue); //=> -3
+
+// console.log(2+2); // =>addition,4
+
+
+// console.log(2-2); //=>substraction,0
+
+
+// console.log(2*2); //=>multiplication,4
+
+
+// console.log(2/2); //=>division,1
+
+
+// console.log(2**3); //=>power,8
+
+
+// console.log(2%2); //=>reminder,0
+
+let str1="Hello"
+let str2="Shivansh"
+let str3=str1+" "+str2
+//console.log(str3);
+
+
+// console.log("1"+2);// 12
+
+
+// console.log(1+"2"); //12
+
+
+// console.log("1"+2+2);// 122,start me string aa gaya to poora statement string ki tarah treat hoga
+
+
+// console.log(1+2+"3");//33,agar string nahi hai to expression solve karke output dega
+
+
+// console.log(3+4*5%3);//5,not a good code
+
+
+// console.log((3+4)*(5%3));//14,good code (always use brackets for long mathematical expression)
+
+// console.log(+true);//1
+// console.log(+false);//0
+ 
+// console.log(+"");//0
+
+// console.log(+null);//0
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
