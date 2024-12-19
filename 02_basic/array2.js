6a0a39db849441faec09daca60d63e6b7dd1d9e6
Your Name <your-email@example.com>
2024-12-19 16:10:04 +0530

    done

--- 02_basic/array2.js
+++ 02_basic/array2.js
@@ -1,26 +1,30 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);//[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
-// console.log(marvel_heros[3][1]);//flash
+/*[CONCAT and PUSH]=>concat aur push dono similar work karte hai array ke case me push same array me changes kar skta hai but concat ke liye hume new array initialize karna hoga*/
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
-// const all_new_heros = [...marvel_heros, ...dc_heros]
+marvel_heros.push(dc_heros)
 
-// console.log(all_new_heros);//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+console.log(marvel_heros);//[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+console.log(marvel_heros[3][1]);//flash
 
-// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
-// const real_another_array = another_array.flat(Infinity)
-// console.log(real_another_array); /*[
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+console.log(all_new_heros);//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)//=>Infinity ka use hum tab karte hai jub array ke andar dher sari arrays ho Infinity sari arrays ke element ko single kar deta hai.
+console.log(real_another_array); /*[
     1, 2, 3, 4, 5,
     6, 7, 6, 7, 4,
     5
-  //
+  */
 
 
 
